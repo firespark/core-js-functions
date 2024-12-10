2209d49263c5b81a17f3793f601e92e108f559f9
fireleit <fireleit@gmail.com>
2024-12-10 16:14:51 +0400

    feat: add solution to function retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -144,9 +144,19 @@ const memoize = (func) => {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
-}
+const retry = (func, attempts) => {
+  return () => {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    throw lastError;
+  };
+};
 
 /**
  * Returns the logging wrapper for the specified method,
